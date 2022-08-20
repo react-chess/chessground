372681f1df18b36ecfe8f2999751cf7473d728b9
H-Richard <r.hong@pm.me>
2022-08-20 16:31:10 -0700

    chore: upgrade chessground to v8.3.5

--- .releaserc.cjs
+++ .releaserc.cjs
@@ -1,4 +1,17 @@
 module.exports = {
-  "branches": ["main"],
-  "repositoryUrl": "https://github.com/react-chess/chessground.git"
+  branches: ["main"],
+  repositoryUrl: "https://github.com/react-chess/chessground.git",
+  plugins: [
+    [
+      "@semantic-release/commit-analyzer",
+      {
+        preset: "angular",
+        releaseRules: [
+          { type: "docs", scope: "README", release: "patch" },
+          { type: "refactor", release: "patch" },
+          { type: "chore", release: "patch" },
+        ],
+      },
+    ],
+  ],
 };
