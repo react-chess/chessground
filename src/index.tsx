e10ec7883a8e0ac9808d47c59db68e795d0e482e
Mike <mike@chessiq.net>
2023-09-27 20:16:21 +0100

    Expose API via useImperativeHandle.

--- src/index.tsx
+++ src/index.tsx
@@ -1,44 +1,66 @@
-import React, { useEffect, useRef, useState } from 'react';
-import { Chessground as ChessgroundApi } from 'chessground';
+import React, { forwardRef, useEffect, useImperativeHandle, useRef, useState } from 'react';
 
-import { Api } from 'chessground/api';
+import { Chessground as ChessgroundApi } from 'chessground';
 import { Config } from 'chessground/config';
+import { Api } from 'chessground/api';
+import { Key } from 'chessground/types';
+
+export type LongAlgebraicMove = `${Key}${Key}`
 
 interface Props {
-  width?: number
-  height?: number
+  width?: number;
+  height?: number;
   contained?: boolean;
-  config?: Config
+  config?: Config;
 }
 
-function Chessground({
-  width = 900, height = 900, config = {}, contained = false,
-}: Props) {
-  const [api, setApi] = useState<Api | null>(null);
+export interface ApiRef {
+  move: (moveStr: LongAlgebraicMove) => void;
+}
 
-  const ref = useRef<HTMLDivElement>(null);
+const Chessground = forwardRef<ApiRef | undefined, Props>(
+  (
+    { width = 900, height = 900, config = {}, contained = false }: Props,
+    apiRef,
+  ) => {
+    const [api, setApi] = useState<Api | null>(null);
+    const divRef = useRef<HTMLDivElement>(null);
 
-  useEffect(() => {
-    if (ref && ref.current && !api) {
-      const chessgroundApi = ChessgroundApi(ref.current, {
-        animation: { enabled: true, duration: 200 },
-        ...config,
-      });
-      setApi(chessgroundApi);
-    } else if (ref && ref.current && api) {
-      api.set(config);
+    const publicApi: ApiRef = {
+      move(moveStr: LongAlgebraicMove) {
+        console.log("[MOVE]", moveStr)
+        api!.move(moveStr.substring(0, 2) as Key, moveStr.substring(2,4) as Key)
+      }
     }
-  }, [ref]);
 
-  useEffect(() => {
-    api?.set(config);
-  }, [api, config]);
+    useImperativeHandle(apiRef, () => {
+      return publicApi;
+    }, [publicApi]);
 
-  return (
-    <div style={{ height: contained ? '100%' : height, width: contained ? '100%' : width }}>
-      <div ref={ref} style={{ height: '100%', width: '100%', display: 'table' }} />
-    </div>
-  );
-}
+    useEffect(() => {
+      if (divRef.current && !api) {
+        const chessgroundApi = ChessgroundApi(divRef.current, {
+          animation: { enabled: true, duration: 200 },
+          ...config,
+        });
+        setApi(chessgroundApi);
+      } else if (divRef.current && api) {
+        api.set(config);
+      }
+    }, [divRef.current, api]);
+
+    useEffect(() => {
+      if (api) {
+        api.set(config);
+      }
+    }, [config]);
+
+    return (
+      <div style={{ height: contained ? '100%' : height, width: contained ? '100%' : width }}>
+        <div ref={divRef} style={{ height: '100%', width: '100%', display: 'table' }} />
+      </div>
+    );
+  }
+);
 
 export default Chessground;
